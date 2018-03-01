011f722f8f063ef9a8fe7b1ba3672b10e19f1add
Your Dev <dev@yourdev.com.br>
2018-03-01 17:10:57 -0300

    php

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600" rel="stylesheet">
+    <link rel="stylesheet" href="css/locou.css">
+    <title>Locou | Encontre o espaço de trabalho pelo tempo que precisar</title>
+    <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body style="font-family: 'Muli'">
+
+    <!-- Navbar -->
+
+    <nav class="navbar fixed-top imagem-escura">
+      <a class="navbar-brand ml-5">
+        <img class="logo-navbar" src="img/locou_logo.png">
+      </a>
+      <span class="navbar-brand menu-navbar mr-5">
+        <a class="mx-3">Sobre</a>
+        <a class="mx-3">Cadastre-se</a>
+        <a class="mx-3">Login</a>
+        <button type="button" class="btn btn-outline-warning">Anuncie Grátis</button>
+      </span>
+    </nav>
+
+    <!-- Seção 1 - Pesquisa -->
+
+    <div class="img-responsive img-center imagem" style="background: url('img/fundo_1.jpg');">
+      <div class="container-fluid text-center justify-content-center imagem-escura">
+
+        <!-- Texto principal + pesquisa -->
+
+        <div class="row">
+          <div class="col-2">
+          </div>
+          <div class="col-8 texto-secao1 secao1">
+            Por 1 hora, 1 tarde, 1 dia:
+            <br>
+            sua nova forma de alugar espaços produtivos sob demanda
+            <br>
+          </div>
+          <div class="col-2">
+          </div>
+        </div>
+
+        <!-- Sub menu -->
+
+        <div class="row imagem-escura py-3">
+          <div class="col-2">
+          </div>
+          <div class="col-8 texto-secao1-categoria">
+            <a class="m-3">Consultórios</a>
+            <a class="m-3">Escritórios</a>
+            <a class="m-3">Cozinhas</a>
+          </div>
+          <div class="col-2">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Seção 2 - Categorias populares -->
+
+    <div class="container-fluid text-center justify-content-center">
+
+      <!-- Titulo Seção -->
+
+      <div class="row secao2">
+        <div class="col-2">
+        </div>
+        <div class="col-8 titulo-secao2">
+          Texto de Categorias
+        </div>
+        <div class="col-2">
+        </div>
+      </div>
+
+      <!-- Cards de categorias -->
+
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-10">
+
+        </div>
+        <div class="col">
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Seção 3 - Ultimas locações -->
+
+    <div class="container-fluid justify-content-center">
+
+      <!-- Titulo Seção -->
+
+      <div class="row secao2">
+        <div class="col-2">
+        </div>
+        <div class="col-8 titulo-secao3 text-center">
+          Texto das ultimas Locações
+        </div>
+        <div class="col-2">
+        </div>
+      </div>
+
+      <!-- Cards Ultimas Locações -->
+
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-10">
+
+          <!-- Linha de cards -->
+
+          <div class="row">
+            <div class="col mx-2" style="background: url('img/item.png')" onClick="window.open( 'https://www.google.com.br','_blank' ); return false;">
+              <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
+              <a class="h6" style="color: #AD8A00">A partir de:</a>
+              <br>
+              <a class="h2">R$ 500</a>
+              </p>
+              <p class="text-center" style="background-color: white;">
+                <img class="rounded-circle" src="img/usuario.jpg" style="height: 60px">
+                <br>
+                <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
+              </p>
+            </div>
+            <div class="col mx-2" style="background: url('img/item.png')">
+              <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
+              <a class="h6" style="color: #AD8A00">A partir de:</a>
+              <br>
+              <a class="h2">R$ 500</a>
+              </p>
+              <p class="text-center" style="background-color: white;">
+                <img class="rounded-circle" src="img/usuario.jpg" style="height: 60px">
+                <br>
+                <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
+              </p>
+            </div>
+            <div class="col mx-2" style="background: url('img/item.png')">
+              <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
+              <a class="h6" style="color: #AD8A00">A partir de:</a>
+              <br>
+              <a class="h2">R$ 500</a>
+              </p>
+              <p class="text-center" style="background-color: white;">
+                <img class="rounded-circle" src="img/usuario.jpg" style="height: 60px">
+                <br>
+                <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
+              </p>
+            </div>
+            <div class="col mx-2" style="background: url('img/item.png')">
+              <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
+              <a class="h6" style="color: #AD8A00">A partir de:</a>
+              <br>
+              <a class="h2">R$ 500</a>
+              </p>
+              <p class="text-center" style="background-color: white;">
+                <img class="rounded-circle" src="img/usuario.jpg" style="height: 60px">
+                <br>
+                <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
+              </p>
+            </div>
+          </div>
+
+        </div>
+        <div class="col">
+        </div>
+      </div>
+    </div>
+
+  </body>
