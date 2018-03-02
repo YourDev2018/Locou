1390911403abf93402e736a097ff9607be8eb2d7
Your Dev <dev@yourdev.com.br>
2018-03-02 11:56:23 -0300

    anuncio card desktop

---
+++ anunciar.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/locou.css">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+    <title>Locou | Anuncie Grátis</title>
+    <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body style="font-family: 'Muli'">
+
+    <nav class="navbar" style="background-color: black">
+      <a class="navbar-brand ml-5">
+        <img class="logo-navbar" src="img/locou_logo.png">
+      </a>
+      <span class="navbar-brand menu-navbar mr-5">
+        <a class="mx-3">Sobre</a>
+        <a class="mx-3">Cadastre-se</a>
+        <a class="mx-3">Login</a>
+        <button type="button" class="btn btn-outline-warning">Anuncie Grátis</button>
+      </span>
+    </nav>
+
+    <!-- Seção Titulo -->
+
+    <div class="container-fluid justify-content-center text-center" style="background-color: #FFC107">
+      <div class="row">
+        <div class="col-4">
+        </div>
+        <div class="col-4 titulo-anuncie">
+          <br>
+          <h1>
+            <b>Texto de anuncio</b>
+            <br>
+          </h1>
+          <h3>
+            Texto de anúncio Texto de anúncio Texto de anúncio Texto de anúncio Texto de anúncio
+          </h3>
+          <br>
+        </div>
+        <div class="col-4">
+        </div>
+      </div>
+    </div>
+
+    <!-- Seção cadastro -->
+
+    <div class="container-fluid justify-content-center text-center">
+      <div class="row">
+        <div class="col-2">
+        </div>
+        <div class="col-8">
+        </div>
+        <div class="col-2">
+        </div>
+      </div>
+    </div>
+
+
+  </body>
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600" rel="stylesheet">
     <link rel="stylesheet" href="css/locou.css">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
     <title>Locou | Encontre o espaço de trabalho pelo tempo que precisar</title>
     <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
@@ -133,7 +134,7 @@
                 <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
               </p>
             </div>
-            <div class="col mx-2" style="background: url('img/item.png')">
+            <div class="col mx-2" style="background: url('img/item.png')" onClick="window.open( 'https://www.google.com.br','_blank' ); return false;">
               <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
               <a class="h6" style="color: #AD8A00">A partir de:</a>
               <br>
@@ -145,7 +146,7 @@
                 <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
               </p>
             </div>
-            <div class="col mx-2" style="background: url('img/item.png')">
+            <div class="col mx-2" style="background: url('img/item.png')" onClick="window.open( 'https://www.google.com.br','_blank' ); return false;">
               <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
               <a class="h6" style="color: #AD8A00">A partir de:</a>
               <br>
@@ -157,7 +158,7 @@
                 <a class="h6" style="color: grey;">Nome do item<br> São Paulo, SP</a>
               </p>
             </div>
-            <div class="col mx-2" style="background: url('img/item.png')">
+            <div class="col mx-2" style="background: url('img/item.png')" onClick="window.open( 'https://www.google.com.br','_blank' ); return false;">
               <p style="margin-top: 65%; background-color: #FFCE00; margin-right: 40%; padding-left: 5%">
               <a class="h6" style="color: #AD8A00">A partir de:</a>
               <br>
@@ -175,6 +176,38 @@
         <div class="col">
         </div>
       </div>
+    </div>
+
+    <!-- Seção 4 - Sobre + Vídeo -->
+
+    <div class="container-fluid justify-content-center text-center secao4">
+      <div class="row">
+        <div class="col-2">
+        </div>
+        <div class="col-8">
+          <h1>Titulo como funciona</h1>
+          <br><br>
+        </div>
+        <div class="col-2">
+        </div>
+      </div>
+
+      <div class="row" style="background-color: #C69F00">
+        <div class="col-2">
+        </div>
+        <div class="col-8">
+          <br><br>
+          <h2>
+            Texto de como funciona <br><br>Texto de como funciona Texto de como funciona Texto de como funciona <span style="color: white">Locou</span> Texto de como funciona Texto de como funciona Texto de como funciona.
+          </h2>
+          <br>
+          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/58VlTRFUKg4?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
+          <br><br>
+        </div>
+        <div class="col-2">
+        </div>
+      </div>
+
     </div>
 
   </body>
