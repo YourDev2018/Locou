8655d8a4f37b26507222f53f0ad0b5b37d329dbf
Your Dev <contato@yourdev.com.br>
2018-04-03 20:47:45 -0300

    att

--- resultado.php
+++ resultado.php
@@ -169,7 +169,7 @@ if (!($tipo == "" || $tipo == null) ) {
         <a class="ml-3"><img class="rounded-circle" src="<?php echo $prefixo.$_SESSION['foto'] ?>" style="height: 40px"></a>
 
         <a class="mx-2"><i style="font-size: 120%" class="far fa-bell"></i></a>
-        <a href="<?php echo "logout.php?pag=resultado"?>" style="color:white" class="mx-2">Logout</a> 
+        <a href="<?php echo "logout.php?pag=resultado"?>" style="color:white" class="mx-2">Logout</a>
       <?php } ?>
     </span>
   </nav>
